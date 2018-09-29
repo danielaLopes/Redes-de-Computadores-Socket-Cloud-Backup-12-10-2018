740e013bfde241ff97c7169113be6e532d5af130
danielaLopes <danielalopes_97@hotmail.com>
2018-09-29 18:51:31 +0100

    initial fork

--- CS.py
+++ CS.py
@@ -2,30 +2,13 @@ import socket
 import sys
 import argparse
 import os
+import multiprocessing
 
 BUFFER_SIZE = 1024
 
 CSname = 'localhost'#socket.gethostname()
 
-if __name__ == "__main__":
-
-	BS_commands = ['REG','RGR', 'UNR', 'UAR', 'LSF', 'LFD N', 'LSU', 'LUR', 'DLB', 'DBR']
-	user_commands = ['AUT', 'AUR', 'DLU', 'DLR', 'BCK', 'BKR', 'RST', 'LSD', 'LDR', 'LSF', 'LFD', 'DEL', 'DDR']
-
-	registered_users = {} # username: password
-	current_user = None
-
-	# Parse argument
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-p', action='store', metavar='CSport', type=int, required=False, default=58018,
-	help='CSport is the well-known port where the CS server accepts user requests, in  TCP. \
-	This  is  an  optional  argument.  If  omitted,  it  assumes  the  value 58000+GN, where\
-	GN is the group number.')
-
-	FLAG = parser.parse_args()
-	CSport = FLAG.p
-
-'''
+def run_UDP_server():
 	UDP_IP = 'localhost'
 	UDP_PORT = 58018
 
@@ -54,65 +37,64 @@ if __name__ == "__main__":
 		    	print('{} {} {}'.format(fields[0], IPBS, portBS))
 		    	udp_socket.sendto('RGR OK'.encode('ascii'), client_address)
 
-
 	except socket.error:
 		print('BS failed to trade data')
 		sys.exit(1)
-'''
-''' TCP CONNECTION '''
 
-try:
-	tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-	print('CS failed to create socket')
-	sys.exit(1)
 
-try:
-	tcp_socket.bind(("", CSport))
-except socket.error:
-	print('CS failed to bind')
-	sys.exit(1)
-
-tcp_socket.listen(5)
-
-while True:
-	# waits for connection
-	print('Waiting for a connection')
+def run_TCP_server():
 	try:
-		connection, client_addr = tcp_socket.accept()
+		tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	except socket.error:
-		print('CS failed to establish connection')
+		print('CS failed to create socket')
 		sys.exit(1)
 
 	try:
-		while True:
-			data = connection.recv(BUFFER_SIZE)
+		tcp_socket.bind(("", CSport))
+	except socket.error:
+		print('CS failed to bind')
+		sys.exit(1)
 
-			if data:
-				fields = data.decode().split()
-				command = fields[0]
+	tcp_socket.listen(5)
 
-				if command in user_commands:
-					if command == 'AUT':
-						username = fields [1]
-						password = fields [2]
+	while True:
+		# waits for connection
+		print('Waiting for a connection')
+		try:
+			connection, client_addr = tcp_socket.accept()
+		except socket.error:
+			print('CS failed to establish connection')
+			sys.exit(1)
 
-						if username in registered_users.keys():
-							if password == registered_users[username]:
-								connection.sendall('AUR OK'.encode('ascii'))
-								current_user = username
-								print('User: "{}"'.format(username))
+		try:
+			while True:
+				data = connection.recv(BUFFER_SIZE)
+
+				if data:
+					fields = data.decode().split()
+					command = fields[0]
+
+					if command in user_commands:
+						if command == 'AUT':
+							username = fields [1]
+							password = fields [2]
+
+							if username in registered_users.keys():
+								if password == registered_users[username]:
+									connection.sendall('AUR OK'.encode('ascii'))
+									current_user = username
+									print('User: "{}"'.format(username))
+								else:
+									connection.sendall('AUR NOK'.encode('ascii'))
+									print('Incorrect password')
 							else:
-								connection.sendall('AUR NOK'.encode('ascii'))
-								print('Incorrect password')
-						else:
-							connection.sendall('AUR NEW'.encode('ascii'))
-							registered_users[username] = password
-							current_user = username
-							print('New user: "{}"'.format(username))
-					# The following messages must be preceded by an AUT message within the same TCP session
-					if (current_user != None): #LEMBRAR DE NO FIM DE CADA SESSAO TCP APAGAR current_user
-						elif(command == 'DLU'):
+								connection.sendall('AUR NEW'.encode('ascii'))
+								registered_users[username] = password
+								current_user = username
+								print('New user: "{}"'.format(username))
+						# The following messages must be preceded by an AUT message within the same TCP session
+						elif (current_user != None): #LEMBRAR DE NO FIM DE CADA SESSAO TCP APAGAR current_user
+							if(command == 'DLU'):
 								print(current_user)
 								#if (current_user.information().isempty()):
 								try:
@@ -123,15 +105,47 @@ while True:
 									print('This user is not registered')
 								#else:
 									#connection.sendall('DLR NOK'.encode('ascii'))
+						else:
+							print('User authentication needed')
 					else:
-						print('User authentication needed')
+						connection.sendall('ERR'.encode('ascii'))
+						sys.exit(1)
 				else:
-					connection.sendall('ERR'.encode('ascii'))
-					sys.exit(1)
-			else:
-				break
-	except socket.error:
-		print('CS failed to trade data')
-		sys.exit(1)
-	finally:
-		connection.close()
+					break
+		except socket.error:
+			print('CS failed to trade data')
+			sys.exit(1)
+		finally:
+			connection.close()
+
+
+if __name__ == "__main__":
+
+	BS_commands = ['REG','RGR', 'UNR', 'UAR', 'LSF', 'LFD N', 'LSU', 'LUR', 'DLB', 'DBR']
+	user_commands = ['AUT', 'AUR', 'DLU', 'DLR', 'BCK', 'BKR', 'RST', 'LSD', 'LDR', 'LSF', 'LFD', 'DEL', 'DDR']
+
+	registered_users = {} # username: password
+	current_user = None
+
+	# Parse argument
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-p', action='store', metavar='CSport', type=int, required=False, default=58018,
+	help='CSport is the well-known port where the CS server accepts user requests, in  TCP. \
+	This  is  an  optional  argument.  If  omitted,  it  assumes  the  value 58000+GN, where\
+	GN is the group number.')
+
+	FLAG = parser.parse_args()
+	CSport = FLAG.p
+
+	# Creating new process to run UDP server
+	try:
+		pid = os.fork()
+	except OSError:
+		exit('CS was unnable to create child process')
+
+	# Child process running UDP server
+	if pid == 0:
+		run_UDP_server()
+	# Parent process running TCP server
+	else:
+		run_TCP_server()
